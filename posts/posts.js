1ae5c689d32c10d7c73cb6fc7eb1fa5445267fcf
madinajabrayil <56426429+madinajabrayil@users.noreply.github.com>
2021-06-09 18:36:23 +0400

    Update posts.js

--- posts/posts.js
+++ posts/posts.js
@@ -1,10 +1,11 @@
 $(document).ready(function() {
     //POST
     //ADD POST
+    // $(".cke_editable > p").addClass("p-editor");
     $(".add-post-btn").click(function(e) {
 
         e.preventDefault();
-        var postText = $(".fr-element p").html();
+        var postText = $(".ck-content").html();
         var postHeading = $(".post-area-heading").val();
 
         if (postHeading.length == 0) {
@@ -15,12 +16,20 @@ $(document).ready(function() {
             alert("Add post heading and content, please.")
         } else {
             $("#post-card-section").append("<div class='post-card card w-75'><div class='card-body'>" +
-                "<h5 class='card-title' >" + postHeading + "</h5> <p class='card-text'>" + postText + "</p > " +
-                " <div class='btn-group'><button type='button' class='btn btn-danger btn-delete'>Sil</button>" +
-                "<button type='button' class='btn btn-warning btn-edit'> Redaktə edin </button>" +
-                "<button type='button' class='btn btn-success btn-pending'> Gözləməyə əlavə edin </button></div></div> </div>")
+                    "<h5 class='card-title' >" + postHeading + "</h5> <span class='card-text'>" + postText + "</span>" +
+                    "<div class='btn-group'><button type='button' class='btn btn-danger btn-delete'>Sil</button>" +
+                    "<button type='button' class='btn btn-warning btn-edit'> Redaktə edin </button>" +
+                    "<button type='button' class='btn btn-success btn-pending'> Gözləməyə əlavə edin </button></div></div> </div>")
+                // $("#post-card-section").append("<div class='post-card card w-75'><div class='card-body'>" +
+                //     "<h5 class='card-title' >" + postHeading + "</h5> " + postText +
+                //     " <div class='btn-group'><button type='button' class='btn btn-danger btn-delete'>Sil</button>" +
+                //     "<button type='button' class='btn btn-warning btn-edit'> Redaktə edin </button>" +
+                //     "<button type='button' class='btn btn-success btn-pending'> Gözləməyə əlavə edin </button></div></div> </div>")
+            $(".post-area-heading").empty();
+            $(".ck-content").empty();
 
         }
+
     })
 
     //DELETE POST
@@ -31,9 +40,10 @@ $(document).ready(function() {
 
     //EDIT POST
     $("body").on('click', '.btn-edit', function() {
-        var postHeading = $(".post-area-heading").val();
-        var postText = $(".fr-element p").text();
-
+        // var postHeading = $(".post-area-heading").val();
+        // var postText = $(".ck-content").html();
+        var postHeading = $(this).parents('div.post-card').find('.card-title').text();
+        var postText = $(this).parents('div.post-card').find('.card-text').html();
 
         $(this).parents('div.post-card').find('.card-title').html(" <textarea name='post-heading' id='post-heading' cols='20' rows='1' class='post-edit-heading' placeholder='Post başlığını redaktə edin..'>" + postHeading + "</textarea>")
         $(this).parents('div.post-card').find('.card-text').html(" <textarea name='post-content' id='post-content' cols='45' rows='5' class='post-edit-content' placeholder='Post məzmununu redaktə edin..'>" + postText + "</textarea>")
@@ -51,12 +61,14 @@ $(document).ready(function() {
         //Get textarea value
         var postHeading = $(this).parents('div.post-card').find('.post-edit-heading').val();
         var postContent = $(this).parents('div.post-card').find('.post-edit-content').val();
-
-        //Update post heading and content
+        // var postContent1 = $(this).parents('div.post-card').find('.post-edit-content').html();
+        // console.log(postContent);
+        // console.log(postContent1)
+        // //Update post heading and content
         $(this).parents('div.post-card').find('.card-title').text(postHeading);
-        $(this).parents('div.post-card').find('.card-text').text(postContent);
+        $(this).parents('div.post-card').find('.card-text').html(postContent);
 
-        //Show edit button, hide update button
+        // //Show edit button, hide update button
         $(this).parents('div.post-card').find('.btn-update').hide();
         $(this).parents('div.post-card').find('.btn-edit').show();
         $(this).parents('div.post-card').find('.btn-pending').show();
